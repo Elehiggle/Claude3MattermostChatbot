fca3427eb6bcc8df2b17da43ddb7c84e416e84c0
Elehiggle <Elehiggle@users.noreply.github.com>
2024-12-14 01:00:21 +0100

    Solving connection issues

---
+++ mattermostdriver_patched.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+import mattermostdriver
+from websocket_patched import PatchedWebsocket
+from config import log_level
+
+logger = logging.getLogger(__name__)
+logger.setLevel(log_level)
+
+mattermostdriver.websocket.Websocket = PatchedWebsocket
+
+if 'mattermostdriver.driver' in sys.modules:
+    del sys.modules['mattermostdriver.driver']
+
+import mattermostdriver.driver  # noqa: E402
+
+mattermostdriver.driver.Websocket = PatchedWebsocket
+
+# Important line for patching
+from mattermostdriver.driver import Driver  # noqa: E402
